a77a07bd0c399c620eede8fafdd491d8b1bf0934
YAMADA Koji <kou64yama@gmail.com>
2016-12-25 13:34:04 +0900

    Create script `npm run build`

--- src/config.js
+++
@@ -1,1 +0,0 @@
-export default port = process.env.PORT || 3000;
--- src/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { port } from './config';
-
-const app = express();
-
-app.listen(port, () => {
-  console.log(`The server is running at http://localhost:${port}/`);
-});
